3fbdfb9e3fb942c01a5c45b75f8e3b7ff1e4f2d9
Diego García <diego.garcia.segura@gmail.com>
2017-06-22 23:55:31 -0600

    Adding hook for iOS to copy entitlements file (currently to use push notifications)

---
+++ hookScripts/ios/copy-entitlements.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(context) {
+    var srcFile = path.join(context.opts.projectRoot, '/platforms/ios/MyApp/Entitlements-Debug.plist');
+    var sourceExists = fs.existsSync(srcFile);
+
+    if(sourceExists) {
+        var destFile = path.join(context.opts.projectRoot, '/platforms/ios/MyApp/MyApp.entitlements');
+        var destExists = fs.existsSync(destFile);
+
+        if(!destExists) {
+            fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
+            console.log('INFO >> iOS entitlements copied');
+        }
+        else {
+            console.log('INFO >> iOS entitlements already exists, copy skiped');
+        }
+    }
+    else {
+        console.log('ERROR >> iOS entitlements file does not exists');
+    }
+}
